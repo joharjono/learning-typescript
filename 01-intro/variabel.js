b1d812c2a0fd47472b4e76c0232d8a5c0a67da44
Jo Harjono <148263321+joharjonocr@users.noreply.github.com>
2024-08-13 19:19:37 +0800

    initial commit

---
+++ 01-intro/variabel.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myName = "jo";
+var number = 5;
+// number.toLowerCase();
+myName.toLowerCase();
+console.log(myName);
+// number
+var userId = 2;
+// userId.toFixed();
+// userId = "dua";
+//boolean
+var isLoogedIn = false;
---
+++ 01-intro/variabel.ts
@@ -0,0 +1,17 @@
+let myName: string = "jo";
+
+let number = 5;
+// number.toLowerCase();
+myName.toLowerCase();
+console.log(myName);
+
+// number
+let userId: number = 2;
+
+// userId.toFixed();
+// userId = "dua";
+
+//boolean
+let isLoogedIn: boolean = false;
+
+export {};
